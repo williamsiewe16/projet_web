d6ea732c075eebc9528987b52c0e8c81a575e2e9
williamsiewe16 <loicosiewe16@gmail.com>
2021-01-07 03:09:20 +0100

    finshing likes and deceptions

--- client/vue-application.js
+++ client/vue-application.js
@@ -10,7 +10,7 @@ const routes = [
   {
     path: '/', component: Main,
     children: [
-      {path: "/accueil", component: Home},
+      {path: "/accuil", component: Home},
       {path: "/likes", component: Likes },
       {path: "/deceptions", component: Deceptions},
       {path: "/contact", component: Contact},
